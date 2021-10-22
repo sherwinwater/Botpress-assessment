dd2f179d3f7fa49785d884c74701f2402494d37a
Jeremy Aube <jeremyaube@pm.me>
2021-10-22 13:12:37 -0400

    Fixed backend to support relative paths and fix file collisions

--- backend/src/data/local-file-repo.ts
+++ backend/src/data/local-file-repo.ts
@@ -4,21 +4,59 @@ import FileRepo from "../domain/repositories/file-repo";
 import chokidar from "chokidar";
 
 export default class LocalFileRepo implements FileRepo {
-  public watchFolder(folderUris: string[]): Observable<FileEvent> {
+  private getWatchedRoot(watchedUris: string[], changedPath: string): string {
+    const changedRootIndex = watchedUris.findIndex((uri) =>
+      changedPath.startsWith(uri)
+    );
+    if (changedRootIndex === -1) {
+      throw new Error("unexpected file change");
+    }
+
+    const changeRoot = watchedUris[changedRootIndex].split("/").reverse()[0];
+    const relativePath = changedPath.replace(
+      watchedUris[changedRootIndex],
+      `[${changedRootIndex}] ${changeRoot}`
+    );
+
+    console.log(relativePath);
+    return relativePath;
+  }
+
+  public watchFolder(folderUris: []): Observable<FileEvent> {
     return new Observable((sub) => {
       const fileWatcher = chokidar.watch(folderUris);
 
       fileWatcher.on("add", (path) =>
-        sub.next(new FileEvent(FileEvents.created, path))
+        sub.next(
+          new FileEvent(
+            FileEvents.created,
+            this.getWatchedRoot(folderUris, path)
+          )
+        )
       );
       fileWatcher.on("unlink", (path) =>
-        sub.next(new FileEvent(FileEvents.deleted, path))
+        sub.next(
+          new FileEvent(
+            FileEvents.deleted,
+            this.getWatchedRoot(folderUris, path)
+          )
+        )
       );
       fileWatcher.on("addDir", (path) =>
-        sub.next(new FileEvent(FileEvents.dirCreated, path))
+        sub.next(
+          new FileEvent(
+            FileEvents.dirCreated,
+            this.getWatchedRoot(folderUris, path)
+          )
+        )
       );
       fileWatcher.on("unlinkDir", (path) =>
-        sub.next(new FileEvent(FileEvents.dirRemoved, path))
+        sub.next(
+          new FileEvent(
+            FileEvents.dirRemoved,
+            this.getWatchedRoot(folderUris, path)
+          )
+        )
       );
     });
   }
--- backend/src/domain/services/file-service.ts
+++ backend/src/domain/services/file-service.ts
@@ -1,15 +1,19 @@
+import * as path from "path";
 import { FileEvent } from "botpress-shared/lib/entities";
 import { Observable } from "rxjs";
 import FileRepo from "../repositories/file-repo";
 
 export default class FileService {
   private fileEventStream?: Observable<FileEvent>;
+  private paths: string[];
 
-  constructor(private folderPaths: string[], private fileRepo: FileRepo) {}
+  constructor(folderPaths: string[], private fileRepo: FileRepo) {
+    this.paths = folderPaths.map((p) => path.resolve(path.normalize(p)));
+  }
 
   public watchFolders(): Observable<FileEvent> {
     if (this.fileEventStream === undefined) {
-      this.fileEventStream = this.fileRepo.watchFolder(this.folderPaths);
+      this.fileEventStream = this.fileRepo.watchFolder(this.paths);
     }
 
     return this.fileEventStream;
